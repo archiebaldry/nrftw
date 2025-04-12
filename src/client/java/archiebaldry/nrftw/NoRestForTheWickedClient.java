9d41a14e4b25a8ca261fedcb6b6b895db41a11df
Archie Baldry <33762310+archiebaldry@users.noreply.github.com>
2025-04-12 09:26:32 +0100

    Initial commit

---
+++ src/client/java/archiebaldry/nrftw/NoRestForTheWickedClient.java
@@ -0,0 +1,10 @@
+package archiebaldry.nrftw;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoRestForTheWickedClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
