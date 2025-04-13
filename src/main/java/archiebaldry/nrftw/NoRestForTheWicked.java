e3f47abdb50c405af280c65169fad6a667325c63
Archie Baldry <33762310+archiebaldry@users.noreply.github.com>
2025-04-13 12:37:52 +0100

    Make drowned drop phantom membranes

--- src/main/java/archiebaldry/nrftw/NoRestForTheWicked.java
+++ src/main/java/archiebaldry/nrftw/NoRestForTheWicked.java
@@ -4,9 +4,18 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.condition.KilledByPlayerLootCondition;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
@@ -20,9 +29,25 @@ public class NoRestForTheWicked implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	@SuppressWarnings("OptionalGetWithoutIsPresent")
+    private static final RegistryKey<LootTable> DROWNED_LOOT_TABLE_ID = EntityType.DROWNED.getLootTableKey().get();
+
+	private static final float PHANTOM_MEMBRANE_CHANCE = 0.25f;
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello, World!");
+
+		LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
+			if (source.isBuiltin() && key.equals(DROWNED_LOOT_TABLE_ID)) {
+				LootPool.Builder poolBuilder = LootPool.builder()
+						.with(ItemEntry.builder(Items.PHANTOM_MEMBRANE)
+								.conditionally(KilledByPlayerLootCondition.builder())
+								.conditionally(RandomChanceLootCondition.builder(PHANTOM_MEMBRANE_CHANCE)));
+
+				tableBuilder.pool(poolBuilder);
+			}
+		});
 
 		UseBlockCallback.EVENT.register(((player, world, hand, hitResult) -> {
 			if (!world.isClient) {
