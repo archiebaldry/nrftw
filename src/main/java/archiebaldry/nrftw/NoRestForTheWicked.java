c1432f99df533612a5083ff892443d279c3b5c1c
Archie Baldry <33762310+archiebaldry@users.noreply.github.com>
2025-05-11 16:07:14 +0100

    Support for MC 1.20.1

--- src/main/java/archiebaldry/nrftw/NoRestForTheWicked.java
+++ src/main/java/archiebaldry/nrftw/NoRestForTheWicked.java
@@ -4,21 +4,20 @@ import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
-import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.KilledByPlayerLootCondition;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.registry.RegistryKey;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameRules;
 import org.slf4j.Logger;
@@ -29,8 +28,7 @@ public class NoRestForTheWicked implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@SuppressWarnings("OptionalGetWithoutIsPresent")
-    private static final RegistryKey<LootTable> DROWNED_LOOT_TABLE_ID = EntityType.DROWNED.getLootTableKey().get();
+    private static final Identifier DROWNED_LOOT_TABLE_ID = EntityType.DROWNED.getLootTableId();
 
 	private static final float PHANTOM_MEMBRANE_CHANCE = 0.25f;
 
@@ -38,8 +36,8 @@ public class NoRestForTheWicked implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Hello, World!");
 
-		LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
-			if (source.isBuiltin() && key.equals(DROWNED_LOOT_TABLE_ID)) {
+		LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+			if (source.isBuiltin() && id.equals(DROWNED_LOOT_TABLE_ID)) {
 				LootPool.Builder poolBuilder = LootPool.builder()
 						.with(ItemEntry.builder(Items.PHANTOM_MEMBRANE)
 								.conditionally(KilledByPlayerLootCondition.builder())
@@ -60,7 +58,7 @@ public class NoRestForTheWicked implements ModInitializer {
 				if (block instanceof BedBlock) {
 					player.sendMessage(Text.literal("There ain't no rest for the wicked"), true);
 
-					LOGGER.info("{} tried to use a bed but there ain't no rest for the wicked.", player.getName().getLiteralString());
+					LOGGER.info("{} tried to use a bed but there ain't no rest for the wicked.", player.getName().getString());
 
 					return ActionResult.SUCCESS;
 				}
